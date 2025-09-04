0f8eaf91ac130230d2c2c35d21a00af286a1c843
rajsri2319 <146314659+rajsri2319@users.noreply.github.com>
2025-09-04 23:41:46 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,6 @@
+import os
 
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///diary.db'  # Use SQLite for simplicity
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your_jwt_secret_key'
