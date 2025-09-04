5f9f2b50572eca50c31d3dca6b927c2949809174
rajsri2319 <146314659+rajsri2319@users.noreply.github.com>
2025-09-04 23:42:45 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,18 @@
+from flask import Flask
+from flask_cors import CORS
+from models import db
+from routes import api
+from config import Config
 
+app = Flask(__name__)
+app.config.from_object(Config)
+CORS(app)  # Enable CORS for all routes
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()  # Create database tables
+
+app.register_blueprint(api, url_prefix='/api')
+
+if __name__ == '__main__':
+    app.run(debug=True)
